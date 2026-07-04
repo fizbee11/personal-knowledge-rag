3c25fcb88d505a35e0e5373bd80089a8284c3264
fizbee11 <fizbee11@gmail.com>
2026-07-04 04:18:49 -0700

    removed redundant code of RAG Indexer

--- main.py
+++ main.py
@@ -1,65 +1,26 @@
 from langchain_core.documents import Document
-from langchain_community.document_loaders import (
-    DirectoryLoader,
-    UnstructuredMarkdownLoader,
-)
-from langchain_text_splitters import RecursiveCharacterTextSplitter
+
 from langchain_ollama import OllamaEmbeddings, ChatOllama
 from langchain.agents import create_agent
-
-from qdrant_client.models import Distance, VectorParams
 from langchain_qdrant import QdrantVectorStore
 from qdrant_client import QdrantClient
-
 from langchain.tools import tool
-
-m_loader_kwargs = {"autodetect_encoding": True}
-
-loader = DirectoryLoader(
-    path="./my_docs",
-    glob="**/*.md",
-    loader_cls=UnstructuredMarkdownLoader,
-    loader_kwargs=m_loader_kwargs,
-)
-docs = loader.load()
-
-
-text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=1000,  # chunk size (characters)
-    chunk_overlap=200,  # chunk overlap (characters)
-    add_start_index=True,  # track index in original document
-)
-all_splits = text_splitter.split_documents(docs)
-
-print(f"Split blog post into {len(all_splits)} sub-documents.")
 
 
 embeddings = OllamaEmbeddings(
     model="nomic-embed-text",
-    validate_model_on_init=False,
+    validate_model_on_init=True,
     base_url="http://zephyrus-1.tailac30a.ts.net:11434",
 )
 
 # Qdrant client
 client = QdrantClient(url="http://localhost:6333")
 
-vector_size = len(embeddings.embed_query("sample text"))
-
-if not client.collection_exists("test"):
-    client.create_collection(
-        collection_name="test",
-        vectors_config=VectorParams(size=vector_size, distance=Distance.COSINE),
-    )
 vector_store = QdrantVectorStore(
     client=client,
-    collection_name="test",
+    collection_name="my_docs",
     embedding=embeddings,
 )
-
-
-document_ids = vector_store.add_documents(documents=all_splits)
-
-print(document_ids[:3])
 
 
 @tool(response_format="content_and_artifact")
@@ -76,7 +37,8 @@ def retrieve_context(query: str):
 llm = ChatOllama(
     model="qwen3:14b",
     temperature=0,
-    validate_model_on_init=True,
+    validate_model_on_init=False,
+    num_ctx=4096,
     base_url="http://zephyrus-1.tailac30a.ts.net:11434",
 )
 
@@ -93,17 +55,12 @@ prompt = (
 agent = create_agent(model=llm, tools=tools, system_prompt=prompt)
 
 
-query = "What is the IP of devminio?\n\nOnce you get the answer, tell mey name also."
+query = "What is the IP of devminio & testminio?\n\nOnce you get the answer, tell mey name  and my phone number also."
 
 inputs = {"messages": [{"role": "user", "content": query}]}
 for chunk in agent.stream(inputs, stream_mode=["messages", "updates"]):
     kind = chunk[0]
     if kind == "messages":
         token, metadata = chunk[1]
-        if token.content:
-            print(token.content, end="", flush=True)
-    elif kind == "updates":
-        for node, update in chunk[1].items():
-            print(f"\n[{node}] {update['messages'][-1]}")
 final_state = agent.invoke(inputs)
 print("\n\nFinal answer:", final_state["messages"][-1].content)
