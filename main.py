ff175c85c86a9f57e8057369976c5e277f0a77db
fizbee11 <fizbee11@gmail.com>
2026-07-03 21:14:43 -0700

    tested vectordb and embeddings

--- main.py
+++ main.py
@@ -5,6 +5,9 @@ from langchain_community.document_loaders import (
 )
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_ollama import OllamaEmbeddings
+from qdrant_client.models import Distance, VectorParams
+from langchain_qdrant import QdrantVectorStore
+from qdrant_client import QdrantClient
 
 m_loader_kwargs = {"autodetect_encoding": True}
 
@@ -27,4 +30,29 @@ all_splits = text_splitter.split_documents(docs)
 print(f"Split blog post into {len(all_splits)} sub-documents.")
 
 
-embeddings = OllamaEmbeddings(model="nomic-embed-text")
+embeddings = OllamaEmbeddings(
+    model="nomic-embed-text",
+    validate_model_on_init=False,
+    base_url="http://zephyrus-1.tailac30a.ts.net:11434",
+)
+
+# Qdrant client
+client = QdrantClient(url="http://localhost:6333")
+
+vector_size = len(embeddings.embed_query("sample text"))
+
+if not client.collection_exists("test"):
+    client.create_collection(
+        collection_name="test",
+        vectors_config=VectorParams(size=vector_size, distance=Distance.COSINE),
+    )
+vector_store = QdrantVectorStore(
+    client=client,
+    collection_name="test",
+    embedding=embeddings,
+)
+
+
+document_ids = vector_store.add_documents(documents=all_splits)
+
+print(document_ids[:3])
